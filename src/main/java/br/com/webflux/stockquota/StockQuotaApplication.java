fed2d5f6f99bfc03b4720cf0cceed36a39d46fb3
Michel Medeiros <michel.tds@gmail.com>
2021-03-13 09:42:12 -0300

    Initial project commit

---
+++ src/main/java/br/com/webflux/stockquota/StockQuotaApplication.java
@@ -0,0 +1,13 @@
+package br.com.webflux.stockquota;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StockQuotaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StockQuotaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/webflux/stockquota/StockQuotaApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.webflux.stockquota;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StockQuotaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
